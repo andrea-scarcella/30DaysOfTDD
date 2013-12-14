6880579a8c827876a2321ffa9929d4a7be1aeca3
Andrea Scarcella <andrea.scarcella.as@gmail.com>
2013-12-14 11:46:26 +0100

    Completed day 4 tutorial

---
+++ ThirtyDaysOfTDD/ThirtyDaysOfTDD.UnitTests/StringUtils.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ThirtyDaysOfTDD.UnitTests
+{
+    public class StringUtils
+    {
+        public int FindNumberOfOccurences(string sentenceToScan, string characterToScanFor)
+        {
+            return (sentenceToScan??"").Where(c => c == (characterToScanFor ?? "").FirstOrDefault()).Count();
+            //throw new NotImplementedException();
+        }
+    }
+}
